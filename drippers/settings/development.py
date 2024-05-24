70599d9ae21b922204828efd1de5a42a98df9b04
Kenneth Danso <kwadwobaahdanso@gmail.com>
2024-05-24 11:38:03 +0000

    feat: database setup postgresql is bein used as a database so i reconfigured the default database to use postgres

--- drippers/settings/development.py
+++ drippers/settings/development.py
@@ -1,1 +1,13 @@
 from .base import *
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": env("DATABASE_ENGINE"),
+        "NAME": env("DATABASE_DB"),
+        "USER": env("DATABASE_USER"),
+        "PASSWORD": env("DATABASE_PASSWORD"),
+        "HOST": env("DATABASE_HOST"),
+        "PORT": env("DATABASE_PORT"),
+    }
+}
