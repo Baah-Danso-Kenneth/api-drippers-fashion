70537851356f1e542a44ca285af2ce4095b92706
Kenneth Danso <kwadwobaahdanso@gmail.com>
2024-05-26 13:56:44 +0000

    feat: mapped the main  urls - Mapped djoser for right path to authenticate users - Include our routers to map to the urls.py

--- drippers/urls.py
+++ drippers/urls.py
@@ -1,22 +1,15 @@
-"""
-URL configuration for drippers project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("api/", include(("apps.router", "apps"), namespace="apps-api")),
+    path("api/v1/auth/", include("djoser.urls")),
+    path("api/v1/auth/", include("djoser.urls.jwt"))
 ]
+
+
+admin.site.site_header = "Fashion World Admin"
+admin.site.site_title = "Drippers Admin Portal"
+admin.site.index_title = "Welcome to the Standout Clique !!!"
+
