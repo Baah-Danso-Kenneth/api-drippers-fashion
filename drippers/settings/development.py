734380fe4a03582e8bbc531653214863a39de87b
Kenneth Danso <kwadwobaahdanso@gmail.com>
2024-05-27 03:04:14 +0000

    feat: email configuration for testing developement

--- drippers/settings/development.py
+++ drippers/settings/development.py
@@ -1,5 +1,15 @@
 from .base import *
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = env("EMAIL_HOST")
+EMAIL_USE_TLS= True
+EMAIL_PORT = env("EMAIL_PORT")
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD =  env("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL= "info@drippers-island.com"
+DOMAIN =  env("DOMAIN")
+SITE_NAME = "Drippers Island"
+
 
 DATABASES = {
     "default": {
