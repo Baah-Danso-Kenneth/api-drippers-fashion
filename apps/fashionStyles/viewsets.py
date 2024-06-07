637298c7fad3d5297913f82af46d91e238346bd3
Kenneth Danso <kwadwobaahdanso@gmail.com>
2024-06-07 13:00:06 +0000

    refactor: viewsets changed to provide unique titles and avoid duplicates

--- apps/fashionStyles/viewsets.py
+++ apps/fashionStyles/viewsets.py
@@ -7,19 +7,17 @@ from rest_framework import viewsets, status
 from rest_framework.decorators import action
 
 
+
 class StylesCategoryViewSet(viewsets.ViewSet):
-    @action(detail=False, methods=['get'])
-    def styles_by_category(self, request):
-        try:
-            categories = Category.objects.all()
-            all_style_category = defaultdict(list)
-            for category in categories:
-                styles = Styles.objects.get()
-                styles_serialized = StyleSerializer(styles, many=True).data
-                all_style_category[category.name].extend(styles_serialized)
 
-            return Response(dict(all_style_category), status=status.HTTP_200_OK)
+    def list(self, request):
+        categories = Category.objects.all()
+        all_titles = set()
 
-        except Exception as e:
-            return Response({'error':str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        # Collect unique titles across all categories
+        for category in categories:
+            styles = Styles.objects.filter(category=category)
+            all_titles.update(style.title for style in styles)
 
+        return Response({"titles": list(all_titles)}, status=status.HTTP_200_OK)
+    
